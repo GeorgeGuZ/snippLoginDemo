1af4b9adb654d6e6699f8a53bcbd32124aa4552e
angular-cli <angular-cli@angular.io>
2017-03-13 09:49:07 -0700

    chore: initial commit from angular-cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snipp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'snipp works!';
+}
